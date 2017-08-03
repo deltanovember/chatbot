fefab9dabb035fc4434e469baebdb2b21dc509c2
coderscollective <don@unswrc.com>
2017-08-03 23:42:33 +1000

    State machine implementation

--- ai.js
+++ ai.js
@@ -4,52 +4,68 @@ var YEARS = 0;
 var START_DATE = 1;
 var RENT = 2;
 
-var state = 0;
+var leaseYears = "";
+var leaseStartDate = "";
+var leaseRent = "";
 
-var decisionTree = {
-	"": "Does your animal have a skeleton?",
-	"0": "Your animal is an insect",
-	"1": "Does your animal have wings?",
-	"10": "Does your animal lay eggs?",	
-	"100": "Does your animal's baby drink milk from its mother?",
-	"1000": "Does your animal go through metamorphosis?",	
-	"10000": "Does your animal have gills?",
-	"100000": "Your animal is a reptile",
-	"100001": "Your animal is a fish",
-	"10001": "Your animal is a reptile",
-	"10001": "Your animal is an amphibian",
-	"100011": "Your animal is a fish",
-	"1001": "Your animal is a mammal",
-	"101": "Does your animal go through metamorphosis?",
-	"1010": "Does your animal have gills?",
-	"10100": "Your animal is a reptile",
-	"10100": "Your animal is a reptile",
-	"10101": "Your animal is a fish",
-	"10101": "Your animal is a fish",
-	"1011": "Your animal is an amphibian",
-	"11": "Does your animal have feathers?",
-	"110": "Your animal is a bat and is a mammal",
-	"111": "Your animal is a bird"
-}
+var state = YEARS;
 
 function robotResponse(userInput) {
 
-	if (userInput.toLowerCase() == 'no') {
-		currentResponse += "0";
-		
- 	} else if (userInput.toLowerCase() == 'yes') {
- 		currentResponse += "1"
+	if (state == YEARS) {
+
+		if (isNumber(userInput)) {
+			response = "Please enter the start date of the lease."
+			leaseYears = userInput;
+			state++;
+		} else {
+			response = "Please enter the number of years in the lease as a number only eg 5"
+		}
+
+ 	} 
+
+ 	else if (state == START_DATE) {
+
+ 		if (isDate(userInput)) {
+ 			response = "Please enter the yearly rent."
+ 			leaseStartDate = userInput;
+ 			state++;
+ 		} else {
+ 			response = "Please enter the date in the following format: DD/MM/YYYY"
+ 		}
  		
- 	} else {
- 		return "Please enter your answer in the form of 'Yes' or 'No'. " + decisionTree[currentResponse];
+
+ 		
+ 	} 
+
+ 	else if (state == RENT) {
+
+ 		if (isCurrency(userInput)) {
+
+ 			leaseRent = userInput;
+ 			response = "I am now preparing the following lease:<br><br>Years: " + leaseYears + " <br>Start date:" + leaseStartDate + "<br>Rent:" + leaseRent;
+	  		$("#btn-input").prop('disabled', true);
+	 		$("#btn-chat").prop('disabled', true);
+
+ 		} else {
+ 			response = "Please enter a dollar amount eg $53,246.35";
+ 		}
  	}
- 	answer = decisionTree[currentResponse];
- 	if (answer.indexOf("Your animal is") != -1) {
- 		$("#btn-input").prop('disabled', true);
- 		$("#btn-chat").prop('disabled', true);
- 	}
-	
-  	return answer;
+
+  	return response;
 }
 
+function isNumber(obj) { 
+	return !isNaN(obj)
+}
 
+function isDate(str) {
+  var m = str.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
+  return (m) ? new Date(m[3], m[2]-1, m[1]) : null;
+}
+
+function isCurrency(str) {
+	var regex = /(?=.)^\$?(([1-9][0-9]{0,2}(,[0-9]{3})*)|0)?(\.[0-9]{1,2})?$/;
+	return regex.test(str);
+
+}
